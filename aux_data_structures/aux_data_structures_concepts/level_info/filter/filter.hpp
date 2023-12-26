f0c1987be0c11ed854733b89bfe0369a489db2fe
arajni3 <fancypirate10@gmail.com>
2023-12-26 17:25:03 -0600

    updated Filter concept

--- aux_data_structures/aux_data_structures_concepts/level_info/filter/filter.hpp
+++ aux_data_structures/aux_data_structures_concepts/level_info/filter/filter.hpp
@@ -4,7 +4,6 @@
 template<typename Filter>
 concept FilterLike = requires(Filter filter, char* key) {
     {filter.insert_key(key)} -> std::same_as<void>;
-    {filter.delete_key(key)} -> std::same_as<void>;
     {filter.prob_contains_key(key)} -> std::same_as<bool>;
-    requires std::default_initializable<Filter>;
+    {filter.empty()} -> std::same_as<bool>;
 };
