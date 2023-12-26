58a5af3dff6debec1c2b6a88f3e5ff09613a52ec
arajni3 <fancypirate10@gmail.com>
2023-12-25 22:21:31 -0600

    improved build_ringdb script and added cmake dirs to gitignore

--- build_ringdb.sh
+++ build_ringdb.sh
@@ -6,8 +6,9 @@ chmod +x build_liburing.sh
 
 # can alternatively set to Release, RelWithDebInfo, or MinSizeRel
 cmake_config=Debug
-# put build files and executable RingDB in ${PWD}/${cmake_config}, where PWD should be the directory for the ringdb project
-/usr/bin/cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=${cmake_config} -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S"${PWD}" -B"${PWD}/${cmake_config}" -G "Unix Makefiles"
-/usr/bin/cmake --build ${PWD}/${cmake_config}--config ${cmake_config} --target all -j 10 --
-sudo chown -R root ${PWD}
-sudo chmod -R 4755 ${PWD}
+build_dir=${PWD}/${cmake_config}
+# put build files and executable RingDB in ${build_dir}, where PWD should be the directory for the ringdb project
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=${cmake_config} -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S"${PWD}" -B"${build_dir}" -G "Unix Makefiles"
+cmake --build ${build_dir} --config ${cmake_config} --target all -j 10 --
+sudo chown -R root ${build_dir}/RingDB
+sudo chmod -R 4755 ${build_dir}/RingDB
