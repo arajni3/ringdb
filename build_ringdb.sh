eeb8b5286bc0f020bb9bae9afdf3576fd5851866
arajni3 <fancypirate10@gmail.com>
2023-12-25 22:47:47 -0600

    added gitignore integration to build_ringdb script

--- build_ringdb.sh
+++ build_ringdb.sh
@@ -7,6 +7,12 @@ chmod +x build_liburing.sh
 # can alternatively set to Release, RelWithDebInfo, or MinSizeRel
 cmake_config=Debug
 build_dir=${PWD}/${cmake_config}
+
+# if build dir name is not in gitignore, append to it
+if [[ "$(grep -cim1 ${cmake_config} .gitignore)" -ge 1 ]]; then
+    echo ${cmake_config} >> .gitignore
+fi 
+
 # put build files and executable RingDB in ${build_dir}, where PWD should be the directory for the ringdb project
 cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=${cmake_config} -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S"${PWD}" -B"${build_dir}" -G "Unix Makefiles"
 cmake --build ${build_dir} --config ${cmake_config} --target all -j 10 --
